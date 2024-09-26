bb91ea57ff372119f0240df65f27eba7a43b195c
raafah pedrao <raafaelamarcelino@gmail.com>
2024-09-26 01:33:17 +0000

    FINALEMNTE COMCLUIDO

--- app/src/main/java/application/model/Jogo.java
+++ app/src/main/java/application/model/Jogo.java
@@ -3,8 +3,6 @@ package application.model;
 import java.util.HashSet;
 import java.util.Set;
  
-import javax.annotation.processing.Generated;
- 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
